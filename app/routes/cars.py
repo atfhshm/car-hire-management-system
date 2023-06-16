717dbcf5e4a39f02fd95ff5a777201f4b920ba0e
atfhshm <its.atfhshm@gmail.com>
2023-06-17 01:02:47 +0300

    add endpoint for retrieving current booking for the day

--- app/routes/cars.py
+++ app/routes/cars.py
@@ -3,12 +3,13 @@ from fastapi.responses import Response
 from psycopg import Connection
 
 from app.db import get_db
+from app.schemas.cars_schemas import CarSchema, BookingSchema
 
 router = APIRouter(prefix="/cars", tags=["cars"])
 
 
 @router.get("/")
-def list_available_cars(db: Connection = Depends(get_db)):
+def list_available_cars(db: Connection = Depends(get_db)) -> list[CarSchema]:
     with db.cursor() as cur:
         cur.execute("SELECT * FROM cars WHERE is_available = true")
         cars = cur.fetchall()
@@ -16,3 +17,23 @@ def list_available_cars(db: Connection = Depends(get_db)):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
     return cars
+
+
+@router.get("/book-car")
+def book_car():
+    ...
+
+
+@router.post("/payment")
+def add_car_payment():
+    ...
+
+
+@router.get("/booking")
+def list_today_booking(db: Connection = Depends(get_db)):
+    with db.cursor() as cur:
+        cur.execute("select * from bookings")
+        bookings = cur.fetchall()
+        if not bookings:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return bookings
---
+++ app/schemas/cars_schemas.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from enum import Enum
+from pydantic import BaseModel
+
+
+class CarType(str, Enum):
+    small_car = "SMALL_CAR"
+    family_car = "FAMILY_CAR"
+    van = "VAN"
+
+
+class CarSchema(BaseModel):
+    id: int
+    company: int
+    model: str
+    type: CarType
+    is_available: bool
+    created_at: datetime
+
+
+class BookingSchema(BaseModel):
+    id: int
+    car: int
+    user: int
+    hire_date: datetime
+    return_date: datetime
+    created_at: datetime
