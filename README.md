664cc4d63e58a8b340dd25ee41012472775aee6a
atfhshm <its.atfhshm@gmail.com>
2023-06-16 14:53:57 +0300

    Configure the initial fastapi project structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# car-hire-management-system
+# Car hire management system
+
+This project describes how to create a small car hire management system, where users (customers) can rent and book different types of cars and vans.
---
+++ app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi import applications
+from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.docs import get_swagger_ui_html
+
+from dataclasses import dataclass
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="./static"), name="static")
+
+
+def swagger_monkey_patch(*args, **kwargs):
+    return get_swagger_ui_html(
+        *args,
+        **kwargs,
+        swagger_js_url="/static/swagger-ui/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui/swagger-ui.css",
+    )
+
+
+applications.get_swagger_ui_html = swagger_monkey_patch
+
+
+@dataclass
+class Message:
+    project: str = "car-hire-management-system"
+    location: str = "root"
+    version: str = "0.0.1"
+
+
+@app.get("/", tags=["root"], response_model=Message)
+def root():
+    return Message()
