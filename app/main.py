78b8814c58272d2aab37541ca86e3ed95bd7ce7d
atfhshm <its.atfhshm@gmail.com>
2023-06-17 08:06:49 +0300

    remove manual swagger-ui

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-from fastapi import applications
-from fastapi.staticfiles import StaticFiles
-from fastapi.openapi.docs import get_swagger_ui_html
 
 from dataclasses import dataclass
 
@@ -136,21 +133,6 @@ def create_payments():
                     """
         )
         db.commit()
-
-
-# app.mount("/static", StaticFiles(directory="./static"), name="static")
-
-
-# def swagger_monkey_patch(*args, **kwargs):
-#     return get_swagger_ui_html(
-#         *args,
-#         **kwargs,
-#         swagger_js_url="/static/swagger-ui/swagger-ui-bundle.js",
-#         swagger_css_url="/static/swagger-ui/swagger-ui.css",
-#     )
-
-
-# applications.get_swagger_ui_html = swagger_monkey_patch
 
 
 @dataclass
