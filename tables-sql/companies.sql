9ef0eb1c4a1eeeafa0d9235fb85aa40821abd041
atfhshm <its.atfhshm@gmail.com>
2023-07-27 12:27:19 +0300

    update and modify equirements.txt file

--- tables-sql/companies.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.companies
-(
-    id serial NOT NULL,
-    name character varying(32) NOT NULL,
-    created_at timestamp with time zone DEFAULT NOW(),
-    PRIMARY KEY (id)
-);
